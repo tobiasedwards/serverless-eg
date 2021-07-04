ffd2302f73b8cace3f6804aaec41b4e783aa3082
Tobias Edwards <tobias.w.edwards@gmail.com>
2021-07-04 16:10:37 +1000

    Re-structure source code

--- serverless_eg/lambda_function.py
+++ serverless_eg/lambda_function.py
@@ -1,5 +1,8 @@
 from typing import Any, Dict
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
+from serverless_eg import __version__
+
+
 def lambda_handler(event: Dict[str, Any], context: LambdaContext):
-    pass
+    print(f"serverless-eg {__version__}")
